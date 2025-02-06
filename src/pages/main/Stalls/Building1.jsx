4aec1bc2b4eb773e50d515533b79057972dd32ff
Jericho <jerichopecho123@gmail.com>
2025-02-06 13:57:34 +0800

    fix the error of merge

--- src/pages/main/Stalls/Building1.jsx
+++ src/pages/main/Stalls/Building1.jsx
@@ -1,19 +1,11 @@
 import PageTitle from "../../../components/main/PageTitle/PageTitle"
 import Building from "../../../components/main/Stalls/Building/Building"
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-=======
->>>>>>> fb5749ce5f7e72d7f1e0135e787758e54dc32498
 const MODELS = [
     { id: 'B1_1-v1', label: '1' },
     // Add more models as needed
   ];
-<<<<<<< HEAD
->>>>>>> fb5749ce5f7e72d7f1e0135e787758e54dc32498
-=======
->>>>>>> fb5749ce5f7e72d7f1e0135e787758e54dc32498
+
 export default function Building1Page(){
     return (
         <>
