1eb9aba03454d436981a9719ce0cf010084ed776
Glenn <gcterrible@gmail.com>
2025-02-06 13:57:16 +0800

    fix changes

--- src/pages/main/Stalls/Building4.jsx
+++ src/pages/main/Stalls/Building4.jsx
@@ -1,19 +1,11 @@
 import PageTitle from "../../../components/main/PageTitle/PageTitle"
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-=======
->>>>>>> fb5749ce5f7e72d7f1e0135e787758e54dc32498
 import Building from "../../../components/main/Stalls/Building/Building"
 const MODELS = [
     { id: 'B4_4-v1', label: '1' },
     // Add more models as needed
   ];
-<<<<<<< HEAD
->>>>>>> fb5749ce5f7e72d7f1e0135e787758e54dc32498
-=======
->>>>>>> fb5749ce5f7e72d7f1e0135e787758e54dc32498
+
 export default function Building4Page(){
     return (
         <>
